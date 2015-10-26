6abb5dccd070eb8fa3f4922c1c810cca0595eae9
Truong Tuan Quang <truong.tuan.quang@framgia.com>
2015-10-26 13:17:23 +0700

    [ADD] Dashboard

--- mooc/urls.py
+++ mooc/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': settings.DEBUG}),
+    
+    url(r'^admin/', include('ars.admin.urls', namespace='admin')),
 ]
