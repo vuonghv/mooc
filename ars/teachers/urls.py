69ef130cfa43ccbba3ea56aebf108473ff241c43
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-28 18:00:19 +0700

    [ADD] Add list teachers

--- ars/teachers/urls.py
+++ ars/teachers/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.ListTeacherView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailTeacherView.as_view(), name='detail'),
 ]
--- ars/teachers/views.py
+++ ars/teachers/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from ars.core.views import BaseView
 from ars.teachers.models import Teacher
@@ -12,9 +12,25 @@ class DetailTeacherView(BaseView, DetailView):
         context = super().get_context_data(**kwargs)
         info = {
             'info': {
-                'title': 'Login - Student'
+                'title': self.object.username,
             },
-            'page_title': 'Login',
+            'page_title': self.object.username,
+        }
+        context.update(info)
+        return context
+
+
+class ListTeacherView(BaseView, ListView):
+    model = Teacher
+    template_name = 'teachers/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        info = {
+            'info': {
+                'title': 'Teachers',
+            },
+            'page_title': 'Teachers',
         }
         context.update(info)
         return context
